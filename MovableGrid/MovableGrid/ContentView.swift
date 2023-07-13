1cc741960f78514f138b94e4f96e21998752afbc
Charlynn <CarberryChai@gmail.com>
2023-07-13 09:06:31 +0800

    remove useless files

---
+++ MovableGrid/MovableGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovableGrid
+//
+//  Created by changlin on 13/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovableGrid/MovableGrid/MovableGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovableGridApp.swift
+//  MovableGrid
+//
+//  Created by changlin on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MovableGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
