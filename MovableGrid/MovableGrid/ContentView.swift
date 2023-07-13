c6b47b6a3bb8e804e6a4b4fd25c70bbb2b74a4cc
Charlynn <CarberryChai@gmail.com>
2023-07-13 19:40:58 +0800

    add some comments

--- MovableGrid/MovableGrid/ContentView.swift
+++ MovableGrid/MovableGrid/ContentView.swift
@@ -21,7 +21,9 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 20)
                             .fill(color.gradient)
                             .frame(height: 100)
+                            // 激活目标对象可拖拽，payload是任何遵循Transferable协议的类型
                             .draggable(color) {
+                                // 在拖拽中提供一个跟随的view或者icon
                                 RoundedRectangle(cornerRadius: 20)
                                     .fill(.ultraThinMaterial)
                                     .frame(width: 1, height: 1)
@@ -29,9 +31,13 @@ struct ContentView: View {
                                         originItem = color
                                     }
                             }
+                            // 承载拖拽目标对象的view
                             .dropDestination(for: Color.self) { _, _ in
+                                // drop之后调用，$0为拖拽目标对象数组[Color], $1为drop点坐标CGPoint
                                 false
                             } isTargeted: { status in
+                                // 拖拽目标对象进入和离开drop区域时调用，status is true when the cursor is inside the area, and
+                                // false when the cursor is outside.
                                 if status, let originItem, originItem != color {
                                     if let originIdx = colors.firstIndex(of: originItem),
                                        let destinationIdx = colors.firstIndex(of: color) {
